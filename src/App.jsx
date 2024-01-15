809020385391f350f2b6ae6e15783b12be118ac7
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-01-15 23:22:24 +0700

    add code for vid-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
 import React from "react";
 
-class ButtonBlack extends React.Component {
-  render() {
-    return (
-      <button className="h-10 px-6 font-semibold rounded-md bg-black text-white">
-        Buy Now
-      </button>
-    );
-  }
-}
+// class ButtonBlack extends React.Component {
+//   render() {
+//     return (
+//       <button className="h-10 px-6 font-semibold rounded-md bg-black text-white">
+//         Buy Now
+//       </button>
+//     );
+//   }
+// }
 
-function ButtonBlue() {
+// function ButtonBlue() {
+//   return (
+//     <button className="h-10 px-6 font-semibold rounded-md bg-blue-600 text-white">
+//       Buy Now
+//     </button>
+//   );
+// }
+
+// const ButtonGreen = () => {
+//   return (
+//     <button className="h-10 px-6 font-semibold rounded-md bg-green-600 text-white">
+//       Buy Now
+//     </button>
+//   );
+// };
+
+const Button = (props) => {
+  const { text = "...", variant = "bg-slate-700" } = props;
   return (
-    <button className="h-10 px-6 font-semibold rounded-md bg-blue-600 text-white">
-      Buy Now
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+    >
+      {text}
     </button>
   );
-}
+};
 
 function App() {
   return (
     <div className="flex justify-center min-h-screen items-center">
-      <div>
-        <ButtonBlack></ButtonBlack>
-        <ButtonBlue></ButtonBlue>
+      <div className="flex gap-x-2">
+        <Button variant="bg-red-700" text="Logout" />
+        <Button variant="bg-green-700" text="Login" />
+        <Button />
       </div>
     </div>
   );
