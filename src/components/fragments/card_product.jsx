0371fedc19d1a99257199ce380a15b5d45ef2295
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-01-16 18:41:57 +0700

    wip: implement usestate

--- src/components/fragments/card_product.jsx
+++ src/components/fragments/card_product.jsx
@@ -2,7 +2,7 @@ import Button from "../elements/button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 my-2 flex flex-col justify-between">
       {children}
     </div>
   );
@@ -32,11 +32,15 @@ const Body = (props) => {
 };
 
 const Footer = (props) => {
-  const { price } = props;
+  const { price, handleAddToCart, id } = props;
   return (
     <div className="flex items-center justify-between px-5 pb-5">
-      <span className="text-xl font-bold text-white">{price}</span>
-      <Button classname="bg-blue-600">Add To Cart</Button>
+      <span className="text-xl font-bold text-white">
+        {price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}
+      </span>
+      <Button classname="bg-blue-600" onClick={() =>   handleAddToCart(id)}>
+        Add To Cart
+      </Button>
     </div>
   );
 };
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,12 +1,12 @@
+import { useState } from "react";
 import Button from "../components/elements/button";
 import CardProduct from "../components/fragments/card_product";
-import Counter from "../components/fragments/counter";
 
 const products = [
   {
     id: 1,
     name: "Sepatu Baru",
-    price: "Rp 1.000.000",
+    price: 1000000,
     image: "images/shoes.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exceptu dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur modi quam molestias.",
@@ -14,7 +14,7 @@ const products = [
   {
     id: 2,
     name: "Sepatu Lama",
-    price: "Rp 7.00.000",
+    price: 700000,
     image: "images/shoes.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exceptu dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur modi quam molestias.",
@@ -22,7 +22,7 @@ const products = [
   {
     id: 3,
     name: "Sepatu Skena",
-    price: "Rp 2.000.000",
+    price: 20000000,
     image: "images/shoes.jpg",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exceptu dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur modi quam molestias.",
@@ -32,10 +32,27 @@ const products = [
 const email = localStorage.getItem("email");
 
 const ProductsPage = () => {
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+    },
+  ]);
+
   const handleLogout = () => {
     localStorage.removeItem("email");
     localStorage.removeItem("password");
     window.location.href = "/login";
+  };
+
+  const handleAddToCart = (id) => {
+    setCart([
+      ...cart,
+      {
+        id,
+        qty: 1,
+      },
+    ]);
   };
   return (
     <>
@@ -46,18 +63,54 @@ const ProductsPage = () => {
         </Button>
       </div>
       <div className="flex justify-center py-5">
-        {products.map((product) => (
-          <CardProduct key={product.id}>
-            <CardProduct.Header image={product.image}></CardProduct.Header>
-            <CardProduct.Body name={product.name}>
-              {product.description}
-            </CardProduct.Body>
-            <CardProduct.Footer price={product.price}></CardProduct.Footer>
-          </CardProduct>
-        ))}
-      </div>
-      <div className="flex w-100 justify-center">
-        <Counter></Counter>
+        <div className="w-3/4 flex flex-wrap">
+          {products.map((product) => (
+            <CardProduct key={product.id}>
+              <CardProduct.Header image={product.image}></CardProduct.Header>
+              <CardProduct.Body name={product.name}>
+                {product.description}
+              </CardProduct.Body>
+              <CardProduct.Footer
+                price={product.price}
+                id={product.id}
+                handleAddToCart={handleAddToCart}
+              ></CardProduct.Footer>
+            </CardProduct>
+          ))}
+        </div>
+        <div className="w-1/4">
+          <h1 className="text-3xl font-bold text-blue-600">Cart</h1>
+          <ul>
+            {cart.map((item) => (
+              <li key={item}>{item.id}</li>
+            ))}
+          </ul>
+          <table>
+            <tr>
+              <thead>
+                <tr>Product</tr>
+                <tr>Price</tr>
+                <tr>Quantity</tr>
+                <tr>Total</tr>
+              </thead>
+              <tbody>
+                {cart.map((item) => {
+                  const product = products.find(
+                    (product) => product.id === item.id
+                  );
+                  return (
+                    <tr key={item.id}>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{item.qty}</td>
+                      <td>{item.qty * product.price}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </tr>
+          </table>
+        </div>
       </div>
     </>
   );
