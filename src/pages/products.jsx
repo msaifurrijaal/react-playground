e8e591f0cb8b934cd3baf2046b78582845b619b8
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-01-16 09:43:19 +0700

    feat: implement rendering list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,26 +1,44 @@
 import CardProduct from "../components/fragments/card_product";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp 1.000.000",
+    image: "images/shoes.jpg",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exceptu dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur modi quam molestias.",
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp 7.00.000",
+    image: "images/shoes.jpg",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exceptu dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur modi quam molestias.",
+  },
+  {
+    id: 3,
+    name: "Sepatu Skena",
+    price: "Rp 2.000.000",
+    image: "images/shoes.jpg",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exceptu dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur modi quam molestias.",
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="images/shoes.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-          dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur
-          modi quam molestias.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000"></CardProduct.Footer>
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="images/shoes.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-          dolorum soluta, labore obcaecati voluptatibus ipsa tempora tenetur
-          modi quam molestias.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000"></CardProduct.Footer>
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image}></CardProduct.Header>
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price}></CardProduct.Footer>
+        </CardProduct>
+      ))}
     </div>
   );
 };
