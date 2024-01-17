eea8d5d70e171de0f72ecf60274b50fea10a71f2
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-01-17 15:17:48 +0700

    feat: get data from api

--- src/components/fragments/card_product.jsx
+++ src/components/fragments/card_product.jsx
@@ -12,7 +12,11 @@ const Header = (props) => {
   const { image } = props;
   return (
     <a href="">
-      <img src={image} alt="products" className="p-8 rounded-t-lg" />
+      <img
+        src={image}
+        alt="products"
+        className="p-8 rounded-t-lg h-60 w-full object-cover"
+      />
     </a>
   );
 };
@@ -23,9 +27,9 @@ const Body = (props) => {
     <div className="px-5 pb-5 h-full">
       <a href="">
         <h5 className="text-xl font-semibold tracking-tight text-white">
-          {name}
+          {name.substring(0, 20)}...
         </h5>
-        <p className="text-sm text-white">{children}</p>
+        <p className="text-sm text-white">{children.substring(0, 100)}</p>
       </a>
     </div>
   );
@@ -36,9 +40,9 @@ const Footer = (props) => {
   return (
     <div className="flex items-center justify-between px-5 pb-5">
       <span className="text-xl font-bold text-white">
-        {price.toLocaleString("id-ID", { style: "currency", currency: "IDR" })}
+        {price.toLocaleString("id-ID", { style: "currency", currency: "USD" })}
       </span>
-      <Button classname="bg-blue-600" onClick={() =>   handleAddToCart(id)}>
+      <Button classname="bg-blue-600" onClick={() => handleAddToCart(id)}>
         Add To Cart
       </Button>
     </div>
